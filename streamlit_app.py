bdb87e005a56754ee64425713293c3bca3d9901e
aggieed97 <aggieed97@gmail.com>
2022-05-14 10:09:38 -0500

    Add Drum Corps colors

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,44 +7,46 @@ import pandas as pd
 import plotly.express as px
 
 import warnings
+
 warnings.filterwarnings("ignore")
 
 url = 'https://en.wikipedia.org/wiki/Drum_Corps_International_World_Class_Champions'
 
-uClient = uReq(url) # opening up connection, grabbing the page
+uClient = uReq(url)  # opening up connection, grabbing the page
 page_html = uClient.read()
 uClient.close()
 page_soup = soup(page_html, "html.parser")
 
 table = page_soup.find_all("table")
-champions = pd.read_html(str(table), header = 0)[0]
+champions = pd.read_html(str(table), header=0)[0]
 
-#https://stackoverflow.com/questions/42205616/using-str-split-for-pandas-dataframe-values-based-on-parentheses-location
+# https://stackoverflow.com/questions/42205616/using-str-split-for-pandas-dataframe-values-based-on-parentheses-location
 champions['Champion & Repertoire'] = champions['Champion & Repertoire'].str.split("\s+\(").str[0]
 
-champions['Champion & Repertoire'] = champions['Champion & Repertoire'].replace('The Cadets of Bergen County', 'The Cadets')
+champions['Champion & Repertoire'] = champions['Champion & Repertoire'].replace('The Cadets of Bergen County',
+                                                                                'The Cadets')
 champions['Champion & Repertoire'] = champions['Champion & Repertoire'].replace('Garfield Cadets', 'The Cadets')
 
-champions = champions[2:].copy().reset_index(drop = True)
+champions = champions[2:].copy().reset_index(drop=True)
 
 ties = [pd.Series([2000, '12 August', 'The Cavaliers', 97.650, 'College Park, Maryland', 'Byrd Stadium'],
-                  index=champions.columns ),
-                pd.Series([1999, '14 August', 'Santa Clara Vanguard', 98.400, 'Madison, Wisconsin', 'Camp Randall Stadium'],
-                          index=champions.columns ),
-                pd.Series([1996, '17 August', 'Phantom Regiment', 97.400, 'Orlando, Florida', 'Citrus Bowl'],
-                          index=champions.columns ),
-       ]
+                  index=champions.columns),
+        pd.Series([1999, '14 August', 'Santa Clara Vanguard', 98.400, 'Madison, Wisconsin', 'Camp Randall Stadium'],
+                  index=champions.columns),
+        pd.Series([1996, '17 August', 'Phantom Regiment', 97.400, 'Orlando, Florida', 'Citrus Bowl'],
+                  index=champions.columns),
+        ]
 
 champions = champions.append(ties, ignore_index=True)
 
-champions = champions.rename(columns = {'Champion & Repertoire':'Drum Corps'})
+champions = champions.rename(columns={'Champion & Repertoire': 'Drum Corps'})
 
 dci_colors = pd.read_csv('drum_corps_colors.csv')
 color_discrete_map = dict(dci_colors.values)
 
 st.set_page_config(page_title="DCI App", layout="wide")
 
-colT1,colT2 = st.columns([1,8])
+colT1, colT2 = st.columns([1, 8])
 with colT2:
     st.title("Drum Corps International")
 
@@ -72,9 +74,9 @@ fig1 = px.histogram(
     xaxis_title='Championship Title Count',
     yaxis_title='Drum Corps',
     font=dict(
-        size = 18,
+        size=18,
         color='white'
-            ),
+    ),
     width=500,
     height=1000,
     showlegend=False,
@@ -84,29 +86,29 @@ fig1 = px.histogram(
         b=100,
         t=100,
         pad=4
-                )
+    )
 ).update_yaxes(
     categoryorder="total ascending"
-).update_traces(hovertemplate ='<i>Championships</i>: ' + '%{x}' + '<extra></extra>',
-                  selector=dict(type="histogram")
-).add_annotation(dict(font=dict(color='white',size=15),
-                                        x=0,
-                                        y=-0.12,
-                                        showarrow=False,
-                                        text="* Including Ties in 1996, 1999, & 2000",
-                                        textangle=0,
-                                        xanchor='left',
-                                        xref="paper",
-                                        yref="paper")
-).add_annotation(dict(font=dict(color='white',size=15),
-                                        x=0.6,
-                                        y=-0.13,
-                                        showarrow=False,
-                                        text="Data: https://en.wikipedia.org/wiki/Drum_Corps_International_World_Class_Champions<br>Graphic:@Danger009Mouse",
-                                        textangle=0,
-                                        xanchor='left',
-                                        xref="paper",
-                                        yref="paper")
-                 )
+).update_traces(hovertemplate='<i>Championships</i>: ' + '%{x}' + '<extra></extra>',
+                selector=dict(type="histogram")
+                ).add_annotation(dict(font=dict(color='white', size=15),
+                                      x=0,
+                                      y=-0.12,
+                                      showarrow=False,
+                                      text="* Including Ties in 1996, 1999, & 2000",
+                                      textangle=0,
+                                      xanchor='left',
+                                      xref="paper",
+                                      yref="paper")
+                                 ).add_annotation(dict(font=dict(color='white', size=15),
+                                                       x=0.6,
+                                                       y=-0.13,
+                                                       showarrow=False,
+                                                       text="Data: https://en.wikipedia.org/wiki/Drum_Corps_International_World_Class_Champions<br>Graphic:@Danger009Mouse",
+                                                       textangle=0,
+                                                       xanchor='left',
+                                                       xref="paper",
+                                                       yref="paper")
+                                                  )
 
 st.plotly_chart(fig1, use_container_width=True)
