fc90ee1d354feed63fa2ea26112f784c468027bb
aggieed97 <aggieed97@gmail.com>
2022-06-11 16:13:44 -0500

    Update main page

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,9 @@ st.set_page_config(page_title="Drum Corps International", layout="wide")
 
 st.title("Today's Predictions")
 
-image = Image.open('images/finals_predictions.png')
+st.markdown("This page is under construction. It currently has pages for Historical Scores and Past DCI Champions. Future plans include adding some DCI Score Analysis as well as Performance predictions.")
+st.markdown('For now, enjoy my 2022 DCI Finals Prediction for Competition that will take place in Indianapolis, IN on August 13, 2022.')
+finals_pred_image = Image.open('images/finals_predictions.png')
 
 col1, col2, col3 = st.columns([3, 5, 0.2])
-col2.image(image, caption='2022 Finals Predictions')
+col2.image(finals_pred_image, caption='2022 Finals Predictions')
