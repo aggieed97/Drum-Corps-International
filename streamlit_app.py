1fef37a22419d57fde6568058552f3bd091582b9
aggieed97 <aggieed97@gmail.com>
2022-06-11 15:58:54 -0500

    Added finals prediction

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,12 @@
 import streamlit as st
 
+from PIL import Image
+
 st.set_page_config(page_title="Drum Corps International", layout="wide")
 
 st.title("Today's Predictions")
+
+image = Image.open('images/finals_predictions.png')
+
+col1, col2, col3 = st.columns([3, 5, 0.2])
+col2.image(image, caption='2022 Finals Predictions')
