fd46c9ce7649bac3b2832c09c414895a37145c0f
aggieed97 <aggieed97@gmail.com>
2022-06-20 18:54:31 -0500

    Redo main page

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,8 +13,4 @@ corps_at_the_crest_image = Image.open('images/corps_at_the_crest_prediction.png'
 
 col1, col2, col3 = st.columns([3, 5, 0.2])
 
-col2.title('August 13, 2022')
 col2.image(finals_pred_image, caption='2022 Finals Predictions, Indianpolis, IN')
-
-col1.title('June 24, 2022')
-col1.image(corps_at_the_crest_image, caption='2022 Corps at the Crest, Vista,CA')
