183f5dceba56b752a14fd8a2a51cb7abdb3daafc
aggieed97 <aggieed97@gmail.com>
2022-06-22 21:13:07 -0500

    Updated Main and Analysis pages

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,13 +4,31 @@ from PIL import Image
 
 st.set_page_config(page_title="Drum Corps International", layout="wide")
 
-st.title("World Class Corps Score Predictions")
+st.title("Drum Corps International")
+st.markdown(
+    """
+    Welcome to my Drum Corps International (DCI) Analysis page.  Here you will find information on \
+    DCI Score Predictions, Historical Scores (2013-2019), as well as Different Types of Analyses.
+    
+    Feel free to browse the site and let me know what you think.  Let us enjoy the 2022 Season Together!
+    
+    1. [Current Predictions](#predictions)
+    1. [Finals Predictions](#finals)
+    1. [Past Predictions](#pastpredictions)
+    """)
 
-st.markdown("This page is under construction. It currently has pages for Historical Scores and Past DCI Champions. Future plans include adding some DCI Score Analysis as well as Performance predictions.")
-st.markdown('For now, enjoy my 2022 DCI Finals Prediction for Competition that will take place in Indianapolis, IN on August 13, 2022.')
 finals_pred_image = Image.open('images/finals_predictions.png')
 corps_at_the_crest_image = Image.open('images/corps_at_the_crest_predictions.png')
 
 col1, col2, col3 = st.columns([3, 5, 0.2])
+
+col1.markdown('<a id="predictions"></a>', unsafe_allow_html=True)
+col1.markdown('## Predictions')
 col1.image(corps_at_the_crest_image, caption='2022 Corps at the Crest Predictions, Vista, CA')
+
+col1.markdown('<a id="finals"></a>', unsafe_allow_html=True)
+col1.markdown('## Finals Predictions')
 col1.image(finals_pred_image, caption='2022 Finals Predictions, Indianpolis, IN')
+
+col1.markdown('<a id="pastpredictions"></a>', unsafe_allow_html=True)
+col1.markdown('## Past Predictions')
