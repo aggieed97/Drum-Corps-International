bc6bb934562bc75640e243158df1f13791ab0a05
aggieed97 <aggieed97@gmail.com>
2022-07-14 10:21:25 -0500

    update front page

--- streamlit_app.py
+++ streamlit_app.py
@@ -138,10 +138,10 @@ fig1 = px.bar(
                                                   )
 st.plotly_chart(fig1, use_container_width=True)
 
-col1.text(" ")
-col1.markdown('<a id="max_rank"></a>', unsafe_allow_html=True)
-col1.markdown("## Rank by Maximum Score To Date")
-col1.image(max_rank_image, caption='Drum Corps Rank by Max Score to Date')
+st.text(" ")
+st.markdown('<a id="max_rank"></a>', unsafe_allow_html=True)
+st.markdown("## Rank by Maximum Score To Date")
+st.image(max_rank_image, caption='Drum Corps Rank by Max Score to Date')
 
 col4, col5, col6 = st.columns([2, 3, 0.2])
 
