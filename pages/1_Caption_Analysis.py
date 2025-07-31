895a30d75d0acc99702b32ef078505fb10bdb614
aggieed97 <aggieed97@gmail.com>
2025-07-31 13:55:51 -0500

    Add Recaps & Fix Caption Analysis

--- pages/1_Caption_Analysis.py
+++ pages/1_Caption_Analysis.py
@@ -8,8 +8,8 @@ color_discrete_map = dict(dci_colors.values)
 
 st.set_page_config(page_title="Caption Analysis", layout="wide")
 
-df = pd.read_csv('pages/2024-DCI-Caption-Score-Recaps.csv')
-larger_df = pd.read_csv('pages/2024-DCI-Caption-Score-Recaps-Large.csv')
+df = pd.read_csv('pages/2025-DCI-Caption-Score-Recaps.csv')
+larger_df = pd.read_csv('pages/2025-DCI-Caption-Score-Recaps-Large.csv')
 
 df['date'] = pd.to_datetime(df.date)
 larger_df['date'] = pd.to_datetime(larger_df.date)
@@ -20,7 +20,7 @@ larger_cols = larger_df.iloc[:, 12:].columns
 df[cols] = df[cols].apply(pd.to_numeric, errors='coerce')
 larger_df[larger_cols] = larger_df[larger_cols].apply(pd.to_numeric, errors='coerce')
 
-corps = list(df.drum_corps.unique())
+corps = list(df.corps.unique())
 corps.sort()
 #corps.insert(0, "All")
 
@@ -29,28 +29,28 @@ captions_list = ['General Effect Total', 'Visual Proficiency', 'Visual Analysis'
 
 
 def ge_scores_by_drum_corps(corp):
-    larger_ge_df = larger_df[larger_df.drum_corps.isin(corp)].reset_index(drop=True)
-    larger_ge_df = larger_ge_df[['date', 'location', 'drum_corps', 'general_effect1_judge', 'general_effect2_judge',
+    larger_ge_df = larger_df[larger_df.corps.isin(corp)].reset_index(drop=True)
+    larger_ge_df = larger_ge_df[['date', 'location', 'corps', 'general_effect1_judge', 'general_effect2_judge',
                                  'general_effect_total_score']]
 
-    ge_df = df[df.drum_corps.isin(corp)].reset_index(drop=True)
+    ge_df = df[df.corps.isin(corp)].reset_index(drop=True)
     ge_df = ge_df[
-        ['date', 'location', 'drum_corps', 'general_effect1_judge', 'general_effect2_judge',
+        ['date', 'location', 'corps', 'general_effect1_judge', 'general_effect2_judge',
          'general_effect_total_score']]
 
     final_df = pd.concat([ge_df, larger_ge_df]).sort_values(by='date')
-    final_df = pd.concat([ge_df, larger_ge_df]).sort_values(by=['drum_corps', 'date'])
+    final_df = pd.concat([ge_df, larger_ge_df]).sort_values(by=['corps', 'date'])
     final_df = final_df[(final_df != 0).all(1)].reset_index(drop=True)
 
     return final_df
     #return ge_df
 
 def music_scores_by_drum_corps(corp='Blue Devils'):
-    music_cols = ['date', 'location', 'drum_corps', 'music_brass_judge', 'music_analysis_judge',
+    music_cols = ['date', 'location', 'corps', 'music_brass_judge', 'music_analysis_judge',
                   'music_percussion_judge', 'music_brass_score', 'music_analysis_score', 'music_percussion_score',
                   'music_total_score']
 
-    larger_music_df = larger_df[larger_df.drum_corps.isin(corp)].reset_index(drop=True)
+    larger_music_df = larger_df[larger_df.corps.isin(corp)].reset_index(drop=True)
 
     if larger_music_df['music_analysis2_score'][0] != 0:
         larger_music_df['music_analysis_score'] = (larger_music_df['music_analysis1_score'] + larger_music_df[
@@ -60,10 +60,10 @@ def music_scores_by_drum_corps(corp='Blue Devils'):
 
     larger_music_df = larger_music_df[music_cols]
 
-    music_df = df[df.drum_corps.isin(corp)].reset_index(drop=True)
+    music_df = df[df.corps.isin(corp)].reset_index(drop=True)
     music_df = music_df[music_cols]
 
-    final_df = pd.concat([music_df, larger_music_df]).sort_values(by=['drum_corps', 'date'])
+    final_df = pd.concat([music_df, larger_music_df]).sort_values(by=['corps', 'date'])
     final_df = final_df[(final_df != 0).all(1)].reset_index(drop=True)
 
     return final_df
@@ -71,56 +71,58 @@ def music_scores_by_drum_corps(corp='Blue Devils'):
 
 
 def visual_scores_by_drum_corps(corp='Blue Devils'):
-    visual_cols = ['date', 'location', 'drum_corps', 'visual_proficiency_judge', 'visual_analysis_judge',
+    visual_cols = ['date', 'location', 'corps', 'visual_proficiency_judge', 'visual_analysis_judge',
                    'visual_color_guard_judge', 'visual_proficiency_score', 'visual_analysis_score',
                    'visual_color_guard_score', 'visual_total_score']
 
-    visual_df = df[df.drum_corps.isin(corp)].reset_index(drop=True)
+    visual_df = df[df.corps.isin(corp)].reset_index(drop=True)
 
     visual_df = visual_df[visual_cols]
 
-    larger_visual_df = larger_df[larger_df.drum_corps.isin(corp)].reset_index(drop=True)
+    larger_visual_df = larger_df[larger_df.corps.isin(corp)].reset_index(drop=True)
     larger_visual_df = larger_visual_df[visual_cols]
 
-    final_df = pd.concat([visual_df, larger_visual_df]).sort_values(by=['drum_corps', 'date'])
+    final_df = pd.concat([visual_df, larger_visual_df]).sort_values(by=['corps', 'date'])
     final_df = final_df[(final_df != 0).all(1)].reset_index(drop=True)
 
     return final_df
     #return visual_df
 
+
 def total_scores_by_drum_corps(corp):
-    larger_ge_df = larger_df[larger_df.drum_corps.isin(corp)].reset_index(drop=True)
+    larger_ge_df = larger_df[larger_df.corps.isin(corp)].reset_index(drop=True)
 
-    larger_ge_df = larger_ge_df[['date', 'location', 'drum_corps', 'total_score']]
+    larger_ge_df = larger_ge_df[['date', 'location', 'corps', 'total_score']]
 
-    ge_df = df[df.drum_corps.isin(corp)].reset_index(drop=True)
-    ge_df = ge_df[['date', 'location', 'drum_corps', 'total_score']]
+    ge_df = df[df.corps.isin(corp)].reset_index(drop=True)
+    ge_df = ge_df[['date', 'location', 'corps', 'total_score']]
 
-    final_df = pd.concat([ge_df, larger_ge_df]).sort_values(by=['drum_corps', 'date'])
+    final_df = pd.concat([ge_df, larger_ge_df]).sort_values(by=['corps', 'date'])
     final_df = final_df[(final_df != 0).all(1)].reset_index(drop=True)
 
     return final_df
     #return ge_df
+
 
 def get_line_chart(caption_picked):
     fig2 = px.line(
         data_frame=filtered_df,
         x='date',
         y=caption_picked,
-        color='drum_corps',
+        color='corps',
         color_discrete_map=color_discrete_map,
         markers=True,
         hover_name='location',
         template='seaborn',
         labels={
-            'drum_corps': 'Drum Corps',
+            'corps': 'Drum Corps',
             'date': 'Date'
         }
     ).update_layout(
         autosize=False,
         width=500,
         height=500,
-        title=f'{caption} Scores for 2024',
+        title=f'{caption} Scores for 2025',
         xaxis_title='Date',
         yaxis_title=f'{caption} Score',
         font=dict(
@@ -130,7 +132,7 @@ def get_line_chart(caption_picked):
                           x=0.85,
                           y=-0.25,
                           showarrow=False,
-                          text="Data: https://dci.org<br>Graphic: @Danger009Mouse",
+                          text="Data: https://dci.org & CorpsClash<br>Graphic: @DangerMouse",
                           textangle=0,
                           xanchor='left',
                           xref="paper",
